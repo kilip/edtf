bf18c5e8d2bd872554c439ad42070049ecf2e23e
Mehmet Coskun <mehmetuw@fastmail.com>
2020-11-02 01:28:28 +0000

    No need to keep start and end in one value object. Instead, two different value object with start and end values

--- src/demo.php
+++ src/demo.php
@@ -6,38 +6,44 @@ require 'edtf/value/EDTFDateTime.php';
 use EDTF\Value\EDTFDateTime as EDTFDateTime;
 
 
-//$dateText = "1985-04-12T21:18:35/2011-07-11T23:51:47+01:32";
-$dateText = "1985-04-12T23:20:30";
+$dateText = "1985-04-12T21:18:35/2011-07-11T23:51:47+01:32";
+//$dateText = "1985-04-12T23:20:30";
+
+echo "DATE TEXT:\n$dateText\n";
+echo "========\n";
 
 $arr = EDTFParser::parseDate( $dateText );
 
 if ( EDTFParser::$isItDatePair ) {
 	$dateTime = new EDTFDateTime( 
-			$arr[0]['second'],	 $arr[1]['second'],
-			$arr[0]['minute'],	 $arr[1]['minute'],
-			$arr[0]['hour'],	 $arr[1]['hour'],
-			$arr[0]['daynum'],	 $arr[1]['daynum'],
-			$arr[0]['monthnum'], $arr[1]['monthnum'],
-			$arr[0]['yearnum'],  $arr[1]['yearnum']
+			$arr[0]['second'],	
+			$arr[0]['minute'],
+			$arr[0]['hour'],
+			$arr[0]['daynum'],
+			$arr[0]['monthnum'],
+			$arr[0]['yearnum']
+		);			
+	echo "START DATE:\n";
+	echo $dateTime;
+	echo "========\n";
+	$dateTime = new EDTFDateTime( 
+			$arr[1]['second'],	
+			$arr[1]['minute'],
+			$arr[1]['hour'],
+			$arr[1]['daynum'],
+			$arr[1]['monthnum'],
+			$arr[1]['yearnum']
 		);
+	echo "END DATE:\n";	
+	echo $dateTime;	
 } else {
 	$dateTime = new EDTFDateTime( 
-			$arr['second'],	 '',
-			$arr['minute'],	 '',
-			$arr['hour'],	 '',
-			$arr['daynum'],	 '',
-			$arr['monthnum'],'',
-			$arr['yearnum'], ''
-		);
+			$arr['second'],	
+			$arr['minute'],
+			$arr['hour'],
+			$arr['daynum'],
+			$arr['monthnum'],
+			$arr['yearnum']
+		);	
+	echo $dateTime;		
 }
-
-print_r($dateTime);
-
-/*
-var_dump( $dateTime );
-*/
-
-/*
-$dateTimeReflection = new ReflectionClass( $dateTime );
-print_r($dateTimeReflection->getProperties() );
-*/
--- src/edtf/value/EDTFDateTime.php
+++ src/edtf/value/EDTFDateTime.php
@@ -4,91 +4,48 @@ namespace EDTF\Value;
 
 final class EDTFDateTime
 {
-
-    private $startSecond;    
-    private $endSecond;
-
-    private $startMinute;    
-    private $endMinute;
 	
-    private $startHour;    
-    private $endHour;	
-	
-    private $startDay;
-    private $endDay;
-
-    private $startMonth;    
-    private $endMonth;
-    
-    private $startYear;    
-    private $endYear;
+    private $second;
+    private $minute;
+    private $hour;
+    private $day;
+    private $month;
+    private $year;
  
 	public function __construct(
-			string $startSecond, string $endSecond,
-			string $startMinute, string $endMinute,
-			string $startHour, string $endHour,
-			string $startDay, string $endDay,
-			string $startMonth, string $endMonth,
-			string $startYear, string $endYear) {
-		
-		$this->startSecond = $startSecond;
-		$this->endSecond   = $endSecond;
-		$this->startMinute = $startMinute;
-		$this->endMinute   = $endMinute;
-		$this->startHour   =  $startHour;
-		$this->endHour	   = $endHour;
-		$this->startDay    = $startDay;
-		$this->endDay	   = $endDay;		
-		$this->startMonth  = $startMonth;
-		$this->endMonth    = $endMonth;
-		$this->startYear   = $startYear;
-		$this->endYear 	   = $endYear;
+		string $second,
+		string $minute,
+		string $hour,
+		string $day,
+		string $month,
+		string $year)
+	{		
+		$this->second = $second;
+		$this->minute = $minute;	
+		$this->hour   = $hour;	
+		$this->day    = $day;
+		$this->month  = $month;
+		$this->year   = $year;	
     }
- 
- 
-	public function getStartSecond(): string { return $this->startSecond;} 
-	public function getEndSecond(): string { return $this->endSecond;}
-
-	public function getStartMinute(): string {return $this->startMinute;}
-	public function getEndMinute(): string {return $this->endMinute;}
-	
-	public function getStartHour(): string {return $this->startHour;}
-	public function getEndHour(): string { return $this->endHour;}
-	
-	public function getStartDay(): string {return $this->startDay;}
-	public function getEndDay(): string { return $this->endDay;}
-
-	public function getStartMonth(): string {return $this->startMonth;}
-	public function getEndMonth(): string {return $this->endMonth;}
-	
-	public function getStartYear(): string {return $this->startYear;} 
-	public function getEndYear(): string {return $this->endYear;}
-	
+  
+	public function getSecond(): string { return $this->second;} 
+	public function getMinute(): string {return $this->minute;}
+	public function getHour(): string {return $this->hour;}
+	public function getDay(): string {return $this->day;}
+	public function getMonth(): string {return $this->month;}
+	public function getYear(): string {return $this->year;}
 	
 	public function __toString() {
 		
 		return
-			"Start Date Second:" . $this->startSecond . "\n" .
-			"End Date Second:"	 . $this->endSecond . "\n" .
-			
-			"Start Date Minute:" . $this->startMinute . "\n" .
-			"End Date Minute:" . $this->endMinute . "\n" .
-			
-			"Start Date Hour:" . $this->startHour . "\n" .
-			"End Date Hour:" . $this->endHour . "\n" .
-			
-			"Start Date Day:" . $this->startDay . "\n" .
-			"End Date Day:" . $this->endDay . "\n" .
-			
-			"Start Date Month:" . $this->startMonth . "\n" .
-			"End Date Month:" . $this->endMonth . "\n" .
-			
-			"Start Date Year:" . $this->startYear . "\n" .
-			"End Date Year:" . $this->endYear . "\n"			
+			"Second:" . $this->second . "\n" .
+			"Minute:" . $this->minute . "\n" .
+			"Hour:"   . $this->hour . "\n" .
+			"Day:"    . $this->day . "\n" .
+			"Month:"  . $this->month . "\n" .
+			"Year:"   . $this->year . "\n"
 			;		
 	}
-
-
 
 /* TODO: Complete equals method
     public function equals(EDTFDateTime $EDTFDateTime): bool

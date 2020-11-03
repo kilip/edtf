5a5e28d31be038ca2f82fdee08d47ae69964e5f1
Mehmet Coskun <mehmetuw@fastmail.com>
2020-11-03 02:10:35 +0000

    Updated readme with a examples for recent changes in the parser and in the library api

--- README.md
+++ README.md
@@ -16,11 +16,19 @@ An example for current parser would be:
 ```php
 <?php
 
-require 'EDTFParser.php';
-require 'edtf/value/EDTFDateTime.php';
+require 'vendor/autoload.php';
 
 use EDTF\Value\EDTFDateTime as EDTFDateTime;
+use EDTF\EDTFParser as EDTFParser;
 
+/*
+Some EDTF formatted date-time examples
+$dateText = "1985-04";
+$dateText = "1985-04-12T21:18:35";
+$dateText = "2004-01-01T10:10:10Z";
+$dateText = "1985-04-12T21:18:35/2011-07-11T23:51:47+01:32";
+$dateText = "1964/2008";
+*/
 
 $dateText = "1985-04-12T21:18:35/2011-07-11T23:51:47+01:32";
 echo "DATE TEXT:\n$dateText\n";
@@ -33,6 +41,13 @@ echo "END DATE:\n";
 echo EDTFParser::getEndDate();
 
 $dateText = "1985-04-12T23:20:30";
+echo "===> DATE TEXT:\n$dateText\n";
+echo "========\n";
+EDTFParser::parseEDTFDate( $dateText );
+echo "ONLY DATE:\n";
+echo EDTFParser::getOnlyDate();
+
+$dateText = "1985-04";
 echo "===> DATE TEXT:\n$dateText\n";
 echo "========\n";
 EDTFParser::parseEDTFDate( $dateText );
--- src/demo.php
+++ src/demo.php
@@ -6,6 +6,14 @@ use EDTF\Value\EDTFDateTime as EDTFDateTime;
 use EDTF\EDTFParser as EDTFParser;
 
 /*
+Some EDTF formatted date-time examples
+$dateText = "1985-04";
+$dateText = "1985-04-12T21:18:35";
+$dateText = "2004-01-01T10:10:10Z";
+$dateText = "1985-04-12T21:18:35/2011-07-11T23:51:47+01:32";
+$dateText = "1964/2008";
+*/
+
 $dateText = "1985-04-12T21:18:35/2011-07-11T23:51:47+01:32";
 echo "DATE TEXT:\n$dateText\n";
 echo "========\n";
@@ -29,29 +37,4 @@ echo "========\n";
 EDTFParser::parseEDTFDate( $dateText );
 echo "ONLY DATE:\n";
 echo EDTFParser::getOnlyDate();
-*/
-
-/*
-//$dateText = "1985-04";
-//$dateText = "1985-04-12T21:18:35";
-//$dateText = "2004-01-01T10:10:10Z";
-echo "===> DATE TEXT:\n$dateText\n";
-echo "========\n";
-EDTFParser::parseEDTFDate( $dateText );
-// print_r( EDTFParser::$parsedEDTFArray );
-echo "ONLY DATE:\n";
-echo EDTFParser::getOnlyDate();
-*/
-
-
-//$dateText = "1985-04-12T21:18:35/2011-07-11T23:51:47+01:32";
-$dateText = "1964/2008";
-echo "DATE TEXT:\n$dateText\n";
-echo "========\n";
-EDTFParser::parseEDTFDate( $dateText );
-echo "===> START DATE:\n";
-echo EDTFParser::getStartDate();
-echo "========\n";
-echo "END DATE:\n";
-echo EDTFParser::getEndDate();
 
